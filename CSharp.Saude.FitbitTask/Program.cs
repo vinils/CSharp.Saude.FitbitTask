980997df30aac5d435765357775f4e7bd8fd604f
vlsantos <vinicius.lourenco@gmail.com>
2019-04-20 00:01:25 -0300

    improving

--- CSharp.Saude.FitbitTask/Program.cs
+++ CSharp.Saude.FitbitTask/Program.cs
@@ -79,5 +79,46 @@
             }
 
         }
+
+        public static void LoadAllSleep(Info info)
+        {
+            var mappedDatas = new List<global::Data.Models.Data>();
+            var sleepData = RequestData.Sleep(info.AcessToken);
+            mappedDatas.AddRange(sleepData.CastToDataDecimal(RequestData.SonoIds));
+
+            DataBulkInsert(info.DataUriService, mappedDatas);
+            mappedDatas = new List<global::Data.Models.Data>();
+        }
+
+        public static void LoadAllHeartRate(Info info)
+        {
+            var date = new DateTime(2016, 1, 1);
+            var mappedDatas = new List<global::Data.Models.Data>();
+            while (date <= DateTime.Today)
+            {
+                for (var timewait = 1; timewait <= 140; timewait++)
+                {
+                    for (var timeinsert = 1; timeinsert <= 5; timeinsert++)
+                    {
+                        var cardioGroupId = new Guid("C0EFE267-E8ED-4B79-A125-DB15ABC0780D");
+                        var heartRateData = RequestData.HeartRate(info.AcessToken, date);
+
+                        if (heartRateData.ActivitiesHeartIntradays != null)
+                        {
+                            mappedDatas.AddRange(heartRateData.ActivitiesHeartIntradays.CastToDataDecimal(cardioGroupId, date));
+                        }
+
+                        System.Diagnostics.Debug.WriteLine(date);
+                        date = date.AddDays(1);
+                    }
+
+                    DataBulkInsert(info.DataUriService, mappedDatas);
+                    mappedDatas = new List<global::Data.Models.Data>();
+                }
+
+                var timeW = new TimeSpan(1, 0, 0);
+                System.Threading.Thread.Sleep(timeW);
+            }
+        }
     }
 }
