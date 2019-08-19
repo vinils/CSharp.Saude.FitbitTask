c70beb98c8b241f8a1e21a1356c5b15ad1a10b9b
vls <vini_ls@hotmail.com>
2019-08-18 23:19:45 -0300

    new feature task scheduler

--- CSharp.Saude.FitbitTask/TaskSchedulerPublish.ps1
+++ CSharp.Saude.FitbitTask/TaskSchedulerPublish.ps1
@@ -1,8 +1,13 @@
-#run build in release
+#run build in release template
 
 mkdir c:\Tasks\
+
 cp .\bin\Release\*.* c:\Tasks\
+cp .\TaskScheduler.xml c:\Tasks\
+
 echo '.\CSharp.Saude.FitbitTask.exe' > c:\Tasks\run.ps1
 echo '$Env:DOCKER_HOST = "tcp://192.168.15.35:2375"' >> c:\Tasks\run.ps1
 echo 'docker exec df3bb739f433 powershell -command "DTExec.exe /f .\import.dtsx"' >> c:\Tasks\run.ps1
 echo "docker exec df3bb739f433 powershell -command 'sqlcmd -Q ""EXEC [DataDW].[dbo].[SP_Reload]""'" >> c:\Tasks\run.ps1
+
+echo "schtasks /query /TN ""Fitbit Task"" >NUL 2>&1 || schtasks /create /xml "".\TaskScheduler.xml"" /tn ""Fitbit Task"" /ru w19temp1\administrator /rp *" >> c:\Tasks\run.ps1
