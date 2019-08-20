28595a148f9733d6fcb009b097f4ead8d3866e95
vls <vini_ls@hotmail.com>
2019-08-19 21:27:14 -0300

    fixing bug

--- CSharp.Saude.FitbitTask/TaskSchedulerPublish.ps1
+++ CSharp.Saude.FitbitTask/TaskSchedulerPublish.ps1
@@ -10,4 +10,4 @@ echo '$Env:DOCKER_HOST = "tcp://192.168.15.35:2375"' >> c:\Tasks\run.ps1
 echo 'docker exec df3bb739f433 powershell -command "DTExec.exe /f .\import.dtsx"' >> c:\Tasks\run.ps1
 echo "docker exec df3bb739f433 powershell -command 'sqlcmd -Q ""EXEC [DataDW].[dbo].[SP_Reload]""'" >> c:\Tasks\run.ps1
 
-echo "schtasks /query /TN ""Fitbit Task"" >NUL 2>&1 || schtasks /create /xml "".\TaskScheduler.xml"" /tn ""Fitbit Task"" /ru w19temp1\administrator /rp *" >> c:\Tasks\run.ps1
+echo "(schtasks /query /TN ""Fitbit Task"") -or (schtasks /create /xml "".\TaskScheduler.xml"" /tn ""Fitbit Task"" /ru w19temp1\administrator /rp *)" >> c:\Tasks\run.ps1
