14ae0f6906f031d7ad1794331cae358cf740ede1
vls <vini_ls@hotmail.com>
2019-08-25 03:41:41 -0300

    improving taskschdule

--- CSharp.Saude.FitbitTask/TaskSchedulerPublish.bat
+++ CSharp.Saude.FitbitTask/TaskSchedulerPublish.bat
@@ -5,7 +5,7 @@ mkdir c:\Tasks\
 copy .\bin\Release\*.* c:\Tasks\
 copy .\TaskScheduler.xml c:\Tasks\
 
-echo c:\Tasks\CSharp.Saude.FitbitTask.exe > c:\Tasks\run.bat
+echo .\CSharp.Saude.FitbitTask.exe > c:\Tasks\run.bat
 echo set DOCKER_HOST=tcp://192.168.15.35:2375 >> c:\Tasks\run.bat
 echo docker exec 5648909f78a8 powershell -command "DTExec.exe /f .\import.dtsx" >> c:\Tasks\run.bat
 echo docker exec 5648909f78a8 powershell -command 'sqlcmd -Q "EXEC [DataDW].[dbo].[SP_Reload]"' >> c:\Tasks\run.bat
