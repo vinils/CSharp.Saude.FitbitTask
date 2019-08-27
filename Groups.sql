56bb4c420e738c7a47d4e845f7d6ffdc2b558a6f
vls <vini_ls@hotmail.com>
2019-08-27 20:41:55 -0300

    add use context to groups

--- Groups.sql
+++ Groups.sql
@@ -1,5 +1,4 @@
-insert into [Group] (Id, Name, Initials, ParentId, MeasureUnit) VALUES (
-'47B974F0-00CC-415B-AF2A-93F010491212', 'Saude', NULL, NULL, NULL)
+USE [DataContext]
 
 --select * from [Group] where ParentId = '47B974F0-00CC-415B-AF2A-93F010491212'
 
