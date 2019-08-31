164afe7b9ef4072b32ba05ca0d56b11db43b59d2
vls <vini_ls@hotmail.com>
2019-08-31 19:25:14 -0300

    improving to get current user and password as parameter

--- CSharp.Saude.FitbitTask/TaskSchedulerPublish.bat
+++ CSharp.Saude.FitbitTask/TaskSchedulerPublish.bat
@@ -10,4 +10,4 @@ echo set DOCKER_HOST=tcp://192.168.15.35:2375 >> c:\Tasks\run.bat
 echo docker exec 5648909f78a8 powershell -command "DTExec.exe /f .\import.dtsx" >> c:\Tasks\run.bat
 echo docker exec 5648909f78a8 powershell -command 'sqlcmd -Q "EXEC [DataDW].[dbo].[SP_Reload]"' >> c:\Tasks\run.bat
 
-echo schtasks /query /TN "Fitbit Task" ^>NUL 2^>^&1 ^|^| schtasks /create /xml ".\TaskScheduler.xml" /tn "Fitbit Task" /ru w19temp1\administrator /rp * >> c:\Tasks\run.bat
+echo schtasks /query /TN "Fitbit Task" ^>NUL 2^>^&1 ^|^| schtasks /create /xml ".\TaskScheduler.xml" /tn "Fitbit Task" /ru %%USERDOMAIN%%\%%USERNAME%% /rp %%1 >> c:\Tasks\run.bat
