09abd66354ae4f6e14a12ee59d34d2201fa9f801
vls <vini_ls@hotmail.com>
2019-12-24 16:44:43 -0300

    improving task

--- CSharp.Saude.FitbitTask/TaskSchedulerPublish.bat
+++ CSharp.Saude.FitbitTask/TaskSchedulerPublish.bat
@@ -6,7 +6,7 @@ copy .\bin\Release\*.* c:\Tasks\
 copy .\TaskScheduler.xml c:\Tasks\
 
 echo .\CSharp.Saude.FitbitTask.exe > c:\Tasks\run.bat
-echo set DOCKER_HOST=tcp://192.168.15.35:2375 >> c:\Tasks\run.bat
+echo set DOCKER_HOST=tcp://w19docker4:2375 >> c:\Tasks\run.bat
 echo docker exec 5648909f78a8 powershell -command "DTExec.exe /f .\import.dtsx" >> c:\Tasks\run.bat
 echo docker exec 5648909f78a8 powershell -command 'sqlcmd -Q "EXEC [DataDW].[dbo].[SP_Reload]"' >> c:\Tasks\run.bat
 
