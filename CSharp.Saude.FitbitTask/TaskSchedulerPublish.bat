5ebe12dca5b88be69c6612e5b0436b249b8ae183
vinils <vini_ls@hotmail.com>
2020-03-23 06:07:58 -0700

    dockerizing fitbittask

--- CSharp.Saude.FitbitTask/TaskSchedulerPublish.bat
+++
@@ -1,3 +0,0 @@
-::run build in release template
-
-echo schtasks /query /TN "Fitbit Task" ^>NUL 2^>^&1 ^|^| schtasks /create /xml ".\TaskScheduler.xml" /tn "Fitbit Task" /ru %%USERDOMAIN%%\%%USERNAME%% /rp %%1 >> c:\Tasks\run.bat
